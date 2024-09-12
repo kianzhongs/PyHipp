4de32047982b82c276390ccf9550930ac204064e
AWS ParallelCluster user <ec2-user@ip-10-0-0-17.ap-southeast-1.compute.internal>
2024-09-12 12:38:46 +0000

    Edited Lab 4 files

--- removefiles.sh
+++ removefiles.sh
@@ -4,11 +4,10 @@ find . -name "*.hkl" -or -name "*.csv" | xargs rm
 
 find . -name "*slurm*err" -or -name "*slurm*out" | xargs rm
 
-rm -r mountains missingData.csv
+rm -r mountains
 
 cd session01
 
 rm binData.hdf logs.txt slist.txt VirtualMaze*
 
 cd ..
-
